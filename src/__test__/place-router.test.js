18a29d2c61e0fd79cb181f2052df5b117cd485f2
cara ottmar <cara.ottmar@gmail.com>
2018-05-03 22:16:31 -0700

    lab built

---
+++ src/__test__/place-router.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import faker from 'faker';
+import superagent from 'superagent';
+import { startServer, stopServer } from '../lib/server';
+import { pCreateCountryMock } from './lib/country-mock';
+import { pCreatePlaceMock, pRemovePlaceMock } from './lib/place-mock';
+
+const apiUrl = `http://localhost:${process.env.PORT}/api/places`;
+
+describe('/api/places', () => {
+  beforeAll(startServer);
+  afterAll(stopServer);
+  afterEach(pRemovePlaceMock);
+
+  describe('POST /api/places', () => {
+    test('200 status code in creation', () => {
+      // Vinicio  - to create a 'real' place I need a mock country
+      return pCreateCountryMock() // Vinicio - Mock
+        .then((countryMock) => {
+          const placeToPost = {
+            region: faker.lorem.words(10),
+            state: faker.lorem.words(11),
+            country: countryMock._id,
+          };
+
+          return superagent.post(apiUrl) // Vinicio - making a real request
+            .send(placeToPost)
+            .then((response) => {
+              expect(response.status).toEqual(200);
+            });
+        });
+    });
+  }); // describe
+  describe('PUT /api/places', () => {
+    test('200 status code in creation', () => {
+      // Vinicio  - to create a 'real' place I need a mock country
+      let placeToUpdate = null;
+      return pCreatePlaceMock()
+        .then((mock) => {
+          placeToUpdate = mock.place;
+          return superagent.put(`${apiUrl}/${mock.place._id}`)
+            .send({ region: 'Pacific Northwest' });
+        })
+        .then((response) => {
+          expect(response.status).toEqual(200);
+          expect(response.body.region).toEqual('Pacific Northwest');
+          expect(response.body.state).toEqual(placeToUpdate.state); // may cause probls
+        });
+    });
+  }); // describe
+});
